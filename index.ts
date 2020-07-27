2a03bff575949a31c975816794c3f22fce0fda82
Muhammad Umar Farooq <muhammadumarfarooq@users.noreply.github.com>
2020-07-27 06:58:00 +0500

    Initial commit with interface

---
+++ index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// Interface in TS is used to define the structure of an object.
+
+const url = "http://jsonplaceholder.typicode.com/posts/1";
+
+interface Post {
+  id: number;
+  title: string
+}
+
+axios.get(url)
+  .then((response) => {
+    const post = response.data as Post;
+    const id = post.id;
+    const title = post.title;
+    logPost(id, title);
+    
+  });
+
+// type annotation
+const logPost = (id: number, title: string) => {
+  console.log(`
+        Post id is: ${id}
+        Post title is: ${title}
+      `);
+}
