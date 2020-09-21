eab4ee20191447bffa2240d8894aa77a38371c35
Muhammad Umar Farooq <umar@orderspot.co>
2020-09-21 07:34:30 +0500

    type annotation around arrays

---
+++ annotations/arrays.ts
@@ -0,0 +1,5 @@
+//we have to write the type annotation explicitly when we initialize an empty array
+// Avoid any type as much as possible
+// const carMakers: string[] = [];
+const carMakers = ['ford', 'toyota', "chevy"];
+
