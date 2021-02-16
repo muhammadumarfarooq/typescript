a65593507efd6b877e98420d398ffc008937cf33
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-02-16 18:16:02 +0500

    another interface type

--- annotations/interfaces.ts
+++ annotations/interfaces.ts
@@ -2,18 +2,25 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean
+
+//  we can define a function like this inside an Iterface. It will return a string.
+  summary(): string
 }
 
 const oldCivic = {
   name: "civic",
   year: 2000,
-  broken: true
+  broken: true,
+  summary: function () {
+    return `Name: ${this.name}`
+  }
 }
 
 const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken: ${vehicle.broken}`);
+  vehicle.summary();
 }
 
 printVehicle(oldCivic);
